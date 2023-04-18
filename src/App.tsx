af00ea9a8680170bba97bf8b4d88f3a898a3481d
Manthan Kuber <manthankuber873@gmail.com>
2023-04-18 12:20:23 +0530

    feat: add background radial

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,39 @@ function App() {
   } = useReactMediaRecorder({ audio: true });
 
   const sendAudio = async () => {
-    const audioBlob = await fetch(mediaBlobUrl as RequestInfo).then((r) =>
-      r.blob()
-    ); //Extract blob
+    const audioBlob = await fetch(mediaBlobUrl!).then((r) => r.blob()); //Extract blob
     const audioFile = new File([audioBlob], "voice.wav", { type: "audio/wav" }); //Convert to audio
     const formData = new FormData();
     formData.append("file", audioFile); // append file to formData
     //TODO: Make api call here
   };
   return (
-    <div className="grid min-h-screen place-items-center">
+    <main className="grid min-h-screen place-items-center">
       <div>
-        <p>{status}</p>
-        <div>
-          <button onClick={startRecording}>Start </button>
+        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px] -z-10" />
+        <p className="text-center" >{status}</p>
+        <div className="flex gap-4 mt-4 justify-center">
+          <button
+            onClick={startRecording}
+            className="p-2 bg-slate-500 text-white rounded-lg border-2 border-transparent  hover:border-sky-500"
+          >
+            Start{" "}
+          </button>
           {/* TODO: Pause / Resume buttons */}
-          <button onClick={pauseRecording}>Pause</button>
-          <button onClick={stopRecording}>Stop </button>
+          <button
+            onClick={pauseRecording}
+            className="p-2 bg-slate-500 text-white rounded-lg border-2 border-transparent  hover:border-sky-500"
+          >
+            Pause{" "}
+          </button>
+          <button
+            onClick={stopRecording}
+            className="p-2 bg-slate-500 text-white rounded-lg border-2 border-transparent  hover:border-sky-500"
+          >
+            Stop{" "}
+          </button>
+        </div>
+        <div className="mt-4" >
           {status === "stopped" && mediaBlobUrl && (
             <>
               <audio src={mediaBlobUrl} controls />
@@ -37,7 +53,7 @@ function App() {
           )}
         </div>
       </div>
-    </div>
+    </main>
   );
 }
 
