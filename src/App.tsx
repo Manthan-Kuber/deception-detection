50516afea96382f1258968be53b59994d0a75aeb
Manthan Kuber <manthankuber873@gmail.com>
2023-04-18 10:27:45 +0530

    feat: add func to convert to wav & send to backend

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,16 @@ function App() {
     pauseRecording,
     mediaBlobUrl,
   } = useReactMediaRecorder({ audio: true });
+
+  const sendAudio = async () => {
+    const audioBlob = await fetch(mediaBlobUrl as RequestInfo).then((r) =>
+      r.blob()
+    ); //Extract blob
+    const audioFile = new File([audioBlob], "voice.wav", { type: "audio/wav" }); //Convert to audio
+    const formData = new FormData();
+    formData.append("file", audioFile); // append file to formData
+    //TODO: Make api call here
+  };
   return (
     <div className="grid min-h-screen place-items-center">
       <div>
@@ -18,7 +28,12 @@ function App() {
           <button onClick={pauseRecording}>Pause</button>
           <button onClick={stopRecording}>Stop </button>
           {status === "stopped" && mediaBlobUrl && (
-            <audio src={mediaBlobUrl} controls />
+            <>
+              <audio src={mediaBlobUrl} controls />
+              <form>
+                <button onClick={sendAudio}>Submit</button>
+              </form>
+            </>
           )}
         </div>
       </div>
