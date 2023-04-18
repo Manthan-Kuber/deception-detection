8d4466964eee6c4711f0384e84e1357c439b5a9d
Manthan Kuber <manthankuber873@gmail.com>
2023-04-18 10:12:34 +0530

    feat: create basic audio recorder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { useReactMediaRecorder } from "react-media-recorder";
 
 function App() {
-
+  const {
+    status,
+    startRecording,
+    stopRecording,
+    pauseRecording,
+    mediaBlobUrl,
+  } = useReactMediaRecorder({ audio: true });
   return (
-    <>
-      
-    </>
+    <div className="grid min-h-screen place-items-center">
+      <div>
+        <p>{status}</p>
+        <div>
+          <button onClick={startRecording}>Start </button>
+          {/* TODO: Pause / Resume buttons */}
+          <button onClick={pauseRecording}>Pause</button>
+          <button onClick={stopRecording}>Stop </button>
+          {status === "stopped" && mediaBlobUrl && (
+            <audio src={mediaBlobUrl} controls />
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
 
