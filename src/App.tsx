057cf85eac44dc9270ed30a23e4b809654d533ec
Manthan Kuber <manthankuber873@gmail.com>
2023-04-20 18:02:38 +0530

    feat: add icons and pass corresponding functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { useReactMediaRecorder } from "react-media-recorder";
+import { RiRecordCircleFill } from "react-icons/ri";
+import { BsPauseCircle, BsStopCircleFill } from "react-icons/bs";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { TiDelete } from "react-icons/ti";
 
 function App() {
   const {
@@ -6,6 +10,8 @@ function App() {
     startRecording,
     stopRecording,
     pauseRecording,
+    resumeRecording,
+    clearBlobUrl,
     mediaBlobUrl,
   } = useReactMediaRecorder({ audio: true });
 
@@ -16,33 +22,42 @@ function App() {
     formData.append("file", audioFile); // append file to formData
     //TODO: Make api call here
   };
+
+  const baseClass = "text-rose-500 hover:cursor-pointer ";
+
   return (
-    <main className="grid min-h-screen place-items-center">
+    <main className="grid min-h-screen place-items-center  bg-slate-800">
       <div>
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px] -z-10" />
-        <p className="text-center" >{status}</p>
-        <div className="flex gap-4 mt-4 justify-center">
-          <button
-            onClick={startRecording}
-            className="p-2 bg-slate-500 text-white rounded-lg border-2 border-transparent  hover:border-sky-500"
-          >
-            Start{" "}
-          </button>
+        <p className="text-center text-white">{status}</p>
+        <div className="flex gap-4 mt-4 justify-center z-40">
           {/* TODO: Pause / Resume buttons */}
-          <button
+          <AiFillPlayCircle
+            className={`${baseClass}`}
+            size={32}
+            onClick={resumeRecording}
+          />
+          <TiDelete
+            className={`${baseClass}`}
+            size={32}
+            onClick={clearBlobUrl}
+          />
+          <RiRecordCircleFill
+            className={`${baseClass}`}
+            size={32}
+            onClick={startRecording}
+          />{" "}
+          <BsPauseCircle
+            className={`${baseClass}`}
+            size={32}
             onClick={pauseRecording}
-            className="p-2 bg-slate-500 text-white rounded-lg border-2 border-transparent  hover:border-sky-500"
-          >
-            Pause{" "}
-          </button>
-          <button
+          />
+          <BsStopCircleFill
+            className={`${baseClass}`}
+            size={32}
             onClick={stopRecording}
-            className="p-2 bg-slate-500 text-white rounded-lg border-2 border-transparent  hover:border-sky-500"
-          >
-            Stop{" "}
-          </button>
+          />
         </div>
-        <div className="mt-4" >
+        <div className="mt-4">
           {status === "stopped" && mediaBlobUrl && (
             <>
               <audio src={mediaBlobUrl} controls />
