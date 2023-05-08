b37abc2626cfdb55f2228afa50ce742da5499f6a
Manthan Kuber <manthankuber873@gmail.com>
2023-05-08 09:45:48 +0530

    style: revamp header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
+import Logo from "./Logo";
+
 const Header = () => {
   return (
-    <header className="p-4 w-full backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
-      Header
+    <header className="p-4 text-lg  text-white w-full backdrop-filter backdrop-blur-sm bg-opacity-10 flex gap-4 items-center border-b">
+      <Logo className=" w-10  h-10" />
+      <span className="block tracking-wider">
+        <span className=" text-xl ">D</span>eception{" "}
+        <span className=" text-xl ">D</span>etector
+      </span>
     </header>
   );
 };
