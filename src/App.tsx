160d9f7c8d68ef2dbe8a2293569275ca71d8fe18
Manthan Kuber <manthankuber873@gmail.com>
2023-05-12 12:28:05 +0530

    feat: add change and upload functions

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import backgroundSvg from "./assets/background.svg";
 import { motion, AnimatePresence } from "framer-motion";
 import RecordAudio from "./components/RecordAudio";
+import Button from "./components/Button";
 
 const serverUrl = import.meta.env.VITE_SERVER_URL;
 
@@ -16,6 +17,8 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [prediction, setPrediction] = useState("");
   const [selectedTab, setSelectedTab] = useState(Tabs[0]);
+  const [selectedFile, setSelectedFile] = useState<File>();
+  const [isFileSelected, setIsFileSelected] = useState(false);
 
   const controller = new AbortController();
 
@@ -42,14 +45,32 @@ function App() {
     prediction,
   };
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedFile(e.target.files![0]);
+    setIsFileSelected(true);
+  };
+
+  const handleUpload = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const { signal } = controller;
+    const formData = new FormData();
+    formData.append("sound", selectedFile!);
+    const options = {
+      signal, //Pass the signal to fetch promise
+      method: "post",
+      body: formData,
+    };
+    sendAudio(options);
+  };
+
   return (
     <div
       className="grid min-h-screen grid-rows-[auto_1fr_auto] bg-slate-800 font-raleway bg-no-repeat bg-cover"
       style={{ backgroundImage: `url(${backgroundSvg})` }}
     >
       <Header />
-      <main className="flex items-center justify-center w-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10">
-        <div className="p-1 border border-neutral-600 min-h-[20rem] min-w-[23rem]  bg-neutral-900 rounded-md">
+      <main className="flex items-center justify-center w-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 px-4">
+        <div className="p-1 border border-neutral-600 w-full min-h-[20rem] max-w-[23rem] max-h-[20rem]   bg-neutral-900 rounded-md">
           <ul className="text-white flex items-center">
             {Tabs.map((tab) => (
               <li
@@ -79,13 +100,24 @@ function App() {
               transition={{ duration: 0.2 }}
             >
               {selectedTab.label === "Record" ? (
-                <div className="flex flex-col items-center" >
+                <div className="flex flex-col items-center">
                   <RecordAudio {...RecordAudioProps} />
                 </div>
               ) : (
-                <div className="text-white grid place-items-center">
-                  <p>Meow</p>
-                </div>
+                <form className="text-white grid place-items-center">
+                  <input
+                    type="file"
+                    accept="audio/*"
+                    name="sound"
+                    onChange={(e) => handleChange(e)}
+                  />
+                  <Button
+                    disabled={!isFileSelected}
+                    onClick={(e) => handleUpload(e)}
+                  >
+                    Upload
+                  </Button>
+                </form>
               )}
             </motion.div>
           </AnimatePresence>
