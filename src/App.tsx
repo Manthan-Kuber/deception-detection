81340d98cd4c2328ec76ec49b53c49c6117a2773
Manthan Kuber <manthankuber873@gmail.com>
2023-05-12 11:03:24 +0530

    feat: add tabs

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import backgroundSvg from "./assets/background.svg";
+import { motion, AnimatePresence } from "framer-motion";
 import RecordAudio from "./components/RecordAudio";
 
 const serverUrl = import.meta.env.VITE_SERVER_URL;
 
+const Tabs = [
+  { id: 0, label: "Record" },
+  { id: 1, label: "Upload" },
+];
+
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [prediction, setPrediction] = useState("");
+  const [selectedTab, setSelectedTab] = useState(Tabs[0]);
 
   const controller = new AbortController();
 
-  //Use the same to upload audio 
+  //Use the same to upload audio
   const sendAudio = async <T extends RequestInit>(options: T) => {
     setIsLoading(true);
     try {
@@ -43,8 +50,33 @@ function App() {
       <Header />
       <main className="flex items-center justify-center w-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10">
         {/* TODO: Create Tabs around wrapper div below*/}
-        <div>
-          <RecordAudio {...RecordAudioProps} />
+        <div className="p-1 border border-neutral-600 h-72 w-72 bg-neutral-900 rounded-md">
+          <ul className="text-white flex items-center">
+            {Tabs.map((tab) => (
+              <li
+                key={tab.id}
+                onClick={() => {
+                  setSelectedTab(tab);
+                }}
+                className="relative hover:cursor-pointer hover:bg-neutral-800 p-2 rounded-tl-md rounded-tr-md flex-1"
+              >
+                {tab.label}
+                {tab === selectedTab && (
+                  <motion.div
+                    className="absolute bg-gradient-to-r inset-x-0 h-1 bottom-0 from-rose-500 to-cyan-300"
+                    layoutId="underline"
+                  />
+                )}
+              </li>
+            ))}
+          </ul>
+          <div className="my-auto py-10 px-4" >
+            {selectedTab.label === "Record" ? (
+                <RecordAudio {...RecordAudioProps} />
+            ) : (
+              <div className="text-white" >Upload</div>
+            )}
+          </div>
         </div>
       </main>
       <Footer />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Logo from "./Logo";
 
 const Header = () => {
   return (
-    <header className="p-4 text-lg  text-white w-full backdrop-filter backdrop-blur-sm bg-opacity-10 flex gap-4 items-center border-b">
+    <header className="p-4 text-lg  text-white w-full backdrop-filter backdrop-blur-sm bg-black/40 flex gap-4 items-center border-b">
       <Logo className=" w-10  h-10" />
       <span className="block tracking-wider">
         <span className=" text-xl ">D</span>eception{" "}
