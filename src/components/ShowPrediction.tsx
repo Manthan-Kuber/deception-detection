b575b8f0a63695926a65b84e2fb8e557f61951bb
Manthan Kuber <manthankuber873@gmail.com>
2023-06-01 11:38:42 +0530

    feat: add prediction string

--- src/components/ShowPrediction.tsx
+++ src/components/ShowPrediction.tsx
@@ -4,7 +4,19 @@ interface Props {
   isLoading: boolean;
   prediction: string;
 }
+
+const getPredictionString = (predNumber: number) => {
+  let predString = "";
+  if (predNumber > 80) predString = "Mostly Truth";
+  if (predNumber > 60 && predNumber < 80) predString = "Probably Truth";
+  if (predNumber > 40 && predNumber < 60)
+    predString = "Probably Lie / Maybe True";
+  if (predNumber < 40) predString = "Mostly Lie";
+  return predString;
+};
+
 const ShowPrediction = ({ isLoading, prediction }: Props) => {
+  const predString = getPredictionString(Number(prediction));
   return (
     <span className="text-white font-mono mt-4 block text-center font-semibold">
       {isLoading ? (
@@ -20,7 +32,18 @@ const ShowPrediction = ({ isLoading, prediction }: Props) => {
           <span>Loading prediction...</span>
         </div>
       ) : (
-        prediction && `Prediction is: ${prediction}`
+        prediction && (
+          <span>
+            <span className="text-lg font-semibold mb-1 bg-clip-text text-transparent bg-gradient-to-r from-rose-500 to-cyan-300">
+              {predString}
+            </span>
+            <br />
+            Truth Probability is:{" "}
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-rose-500 to-cyan-300">
+              {prediction} %{" "}
+            </span>
+          </span>
+        )
       )}
     </span>
   );
